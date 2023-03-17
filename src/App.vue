35e489d5cb120a29e3653414b16842d58e5582cd
mrVandermond <kirill.guyda32@gmail.com>
2023-03-17 17:47:12 +0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <VBtn color="primary">Hello</VBtn>
+
+  <RouterView />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+import App from "./App.vue";
+import router from "./router";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const DB = getDatabase(firebaseApp);
+
+void set(ref(DB, 'users/2'), {
+  name: 'name',
+  surname: 'surname',
+});
+
+const vuetify = createVuetify();
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
