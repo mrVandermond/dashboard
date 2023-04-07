bc942e85c019372e998629ffc5eff4b3d0ec6770
Victor <uberman59@gmail.com>
2023-04-07 18:22:23 +0500

    feat: loginView beta

---
+++ src/views/LoginView.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-row justify="center" align="center" class="fill-height">
+    <v-col class="flex-grow-1" cols="12" sm="6" md="4">
+      <v-card>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="email"
+              label="Email"
+              type="email"
+              :rules="[emailRules]"
+              required
+            />
+
+            <v-text-field
+              v-model="password"
+              label="Password"
+              type="password"
+              :rules="[passwordRules]"
+              required
+            />
+
+            <v-row>
+              <v-col cols="12" class="text-center">
+                <v-btn color="primary" @click.prevent="login"> Login </v-btn>
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col cols="12" class="text-center">
+                <v-btn text @click="forgotPassword"> Forgot password? </v-btn>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const email = ref('');
+    const password = ref('');
+
+    const emailRules = [
+      (v) => !!v || 'E-mail is required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ];
+
+    const passwordRules = [
+      (v) => !!v || 'Password is required',
+      (v) => v.length >= 8 || 'Password must be at least 8 characters',
+    ];
+
+    function login() {
+      // Добавьте ваш код для обработки логина пользователя
+      // e.g. отправка формы с учетными данными на сервер
+    }
+
+    function forgotPassword() {
+      //Добавьте ваш код для обработки забытого пароля
+      //e.g. перенаправление на страницу сброса пароля
+    }
+
+    return {
+      email,
+      password,
+      emailRules,
+      passwordRules,
+      login,
+      forgotPassword,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped></style>
