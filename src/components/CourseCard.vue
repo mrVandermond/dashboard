6fb2e9b1bc8ab4100298178ffb5b502481a491d8
Dmitry Razumny <nepalevnodigital@gmail.com>
2023-04-29 16:21:12 +0300

    fix codestyle

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,37 +1,66 @@
 <template>
-    <v-card class="mx-auto" color="white" theme="dark" max-width="400" :prepend-icon="course.icon" :title="course.name">
-        <template v-slot:prepend>
-            <v-icon size="x-large"></v-icon>
+    <v-card
+    class="mx-auto"
+    color="white"
+    theme="dark"
+    max-width="400"
+    :prepend-icon="course.icon"
+    :title="course.name">
+        <template
+        v-slot:prepend>
+            <v-icon
+            size="x-large">
+            </v-icon>
         </template>
 
-        <v-card-text class="text-h5 py-2">
+        <v-card-text
+        class="text-h5 py-2">
             "{{ course.description }}" <br>
             <v-container>
                 <v-row>
                     <v-col>
-                        <v-card color="white" theme="dark">
+                        <v-card
+                        color="white"
+                        theme="dark">
                             {{ course.deadline }}
                         </v-card>
                     </v-col>
                     <v-col>
-                        <v-card color="white" theme="dark">
+                        <v-card
+                        color="white"
+                        theme="dark">
                             {{ course.budget }}
                         </v-card>
                     </v-col>
                 </v-row>
             </v-container>
 
-            <v-progress-linear style="margin-top: 20px;" :model-value="course.progress" :height="7" rounded rounded-bar
-                color="blue" />
+            <v-progress-linear
+            style="margin-top: 20px;"
+            :model-value="course.progress"
+            :height="7"
+            rounded rounded-bar
+            color="blue"
+            />
 
         </v-card-text>
 
         <v-card-actions>
-            <v-list-item class="w-100">
-                <template v-slot:prepend>
-                    <v-avatar color="grey-darken-3" :image="user.avatarLink"></v-avatar>
-                    <v-avatar color="grey-darken-3" :image="user.avatarLink"></v-avatar>
-                    <v-avatar color="grey-darken-3" :image="user.avatarLink"></v-avatar>
+            <v-list-item
+            class="w-100">
+                <template
+                v-slot:prepend>
+                    <v-avatar
+                    color="grey-darken-3"
+                    :image="user.avatarLink">
+                    </v-avatar>
+                    <v-avatar color="grey-darken-3"
+                    :image="user.avatarLink">
+                    </v-avatar>
+                    <v-avatar
+                    color="grey-darken-3"
+                    :image="user.avatarLink">
+                    </v-avatar>
                 </template>
             </v-list-item>
         </v-card-actions>
@@ -53,4 +82,5 @@ const user = {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+</style>
