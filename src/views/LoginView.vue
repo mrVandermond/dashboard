ad7416ca441188d9be610f38e5bf34955e351d30
Victor <uberman59@gmail.com>
2023-05-05 18:10:34 +0500

    feat: loginPage trial version complete

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,12 +12,12 @@
               required
             />
 
-            <v-text-field
+            <!-- <v-text-field
               v-model="password"
               label="Password"
               type="password"
               :rules="[passwordRules]"
-            />
+            /> -->
 
             <v-row>
               <v-col cols="12" class="text-center">
@@ -25,11 +25,11 @@
               </v-col>
             </v-row>
 
-            <v-row>
+            <!-- <v-row>
               <v-col cols="12" class="text-center">
                 <v-btn text @click="forgotPassword"> Forgot password? </v-btn>
               </v-col>
-            </v-row>
+            </v-row> -->
           </v-form>
         </v-card-text>
       </v-card>
@@ -39,21 +39,7 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-const DB = getDatabase(firebaseApp);
+import { checkEmailExists } from '@/firebase/checkUserIsExists.ts';
 
 const router = useRouter();
 
@@ -65,32 +51,23 @@ const emailRules = [
   (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
 ];
 
-const passwordRules = [
-  (v) => !!v || 'Password is required',
-  (v) => v.length >= 8 || 'Password must be at least 8 characters',
-];
+// const passwordRules = [
+//   (v) => !!v || 'Password is required',
+//   (v) => v.length >= 8 || 'Password must be at least 8 characters',
+// ];
 
-function login() {
-  // const emailRef = firebase
-  //   .database()
-  //   .ref('users')
-  //   .orderByChild('email')
-  //   .equalTo(email.value);
-  // emailRef.once('value', (snapshot) => {
-  //   if (snapshot.exists()) {
-  //     // email найден в базе данных
-  //     // перенаправляем пользователя на главную страницу
-  //     router.push('/');
-  //   } else {
-  //     // email не найден в базе данных
-  //     // выдаем ошибку
-  //     alert('Email not found');
-  //   }
-  // });
+async function login() {
+  if (!email.value) return;
+  const result = await checkEmailExists(email.value);
+
+  if (result) {
+    return await router.push('/');
+  }
+  console.warn(email.value, 'this email not found');
 }
-function forgotPassword() {
-  //Добавьте ваш код для обработки забытого пароля
-  //e.g. перенаправление на страницу сброса пароля
-}
+// function forgotPassword() {
+//   //Добавьте ваш код для обработки забытого пароля
+//   //e.g. перенаправление на страницу сброса пароля
+// }
 </script>
 <style lang="scss" scoped></style>
